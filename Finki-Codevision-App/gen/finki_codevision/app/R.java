3606a55cdc88bc1335557618134f1c56e89d84a1
Riste <rikibaco@outlook.com>
2013-10-07 23:49:30 +0200

    button style defined

--- Finki-Codevision-App/gen/finki_codevision/app/R.java
+++ Finki-Codevision-App/gen/finki_codevision/app/R.java
@@ -165,6 +165,7 @@ public final class R {
         public static final int ic_jazik=0x7f020001;
         public static final int ic_kod=0x7f020002;
         public static final int ic_launcher=0x7f020003;
+        public static final int ic_predmet=0x7f020004;
     }
     public static final class id {
         public static final int IzberiJazikbtn=0x7f090003;
